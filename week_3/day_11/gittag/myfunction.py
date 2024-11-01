a34da6065a736fcfc64c6de8d1f0e90d3c176cc5
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-11-01 17:03:36 +0100

    feat: add gittag assignment

---
+++ week_3/day_11/gittag/myfunction.py
@@ -0,0 +1,13 @@
+"""Dummy function"""
+
+def print_hello():
+    """Dummy funtion to print hello."""
+    print("hello")
+
+def second_function():
+    """This is a second dummy function."""
+    print("Tag v1.1.0")
+
+def third_function():
+    """This is a third dummy function."""
+    print_hello("This is a develop branch from tag 1.1.0")
